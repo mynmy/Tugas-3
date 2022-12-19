3a4db11253350b2b39ead785029fcfaca70eb7fa
mynmy <118150671+mynmy@users.noreply.github.com>
2022-12-19 10:39:35 +0700

    Update Tugas 3 - Piramida.c

--- Tugas 3 - Piramida.c
+++ Tugas 3 - Piramida.c
@@ -1,29 +1,35 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
-  int i, j, height;
+    //inisialisasi variabel
+    int j, i, k, n, n1, n2;
+    
 
-  printf("Masukkan tinggi piramida: ");
-  scanf("%d", &height);
-
-  if (height % 2 == 0) {
-    printf("Tinggi piramida harus bilangan ganjil!\n");
-    return 1;
-  }
-
-  for (i = 1; i <= height; i++) {
-    for (j = 1; j <= i; j++) {
-      printf("* ");
+    printf("Masukan Tinggi Bintang : "); scanf("%d",&n);
+    //mencari nilai tengah
+    n1=n/2+1;
+    n2=n1-1;
+    
+    if (n % 2 == 1){
+        for(i=1; i<=n; i++){
+            for(j=1; j<=i; j++){
+                printf("* "); }
+                printf("\n");
+                
+            if(i==n1){
+                for(k=n2;k>=1;k--){
+                    for(j=1;j<=k;j++){
+                     printf("* ");
+                    }
+                     printf("\n");
+                     i++;
+                }
+            }  
+           
+        }  
+    } else {
+        printf("Kesalahan Input !!! \nmasukan bilangan ganjil");
     }
-    printf("\n");
-  }
-
-  for (i = height - 1; i >= 1; i--) {
-    for (j = 1; j <= i; j++) {
-      printf("* ");
-    }
-    printf("\n");
-  }
-
-  return 0;
+    return 0;
 }
